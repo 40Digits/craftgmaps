1a2dc2fdbb26f66cfb776cbc9745e518c00a13d8
David Hahn <david@40digits.com>
2016-03-12 11:31:16 -0600

    Add drag listener to marker closes #3

--- resources/js/input.js
+++ resources/js/input.js
@@ -3,6 +3,7 @@ function googleMapify(formattedAddressId, mapId, latId, lngId, defaultLat, defau
   var mapEl = document.getElementById(mapId);
   var latEl = document.getElementById(latId);
   var lngEl = document.getElementById(lngId);
+  var geocoder = new google.maps.Geocoder();
 
   if (!!latEl.value && !!lngEl.value) {
     var mapCenter = { 
@@ -27,24 +28,38 @@ function googleMapify(formattedAddressId, mapId, latId, lngId, defaultLat, defau
   
   var marker = new google.maps.Marker({
     map: map,
+    draggable: true,
     position: mapCenter,
   });
 
-  var geocodeSearch = function(address) {
-    new google.maps.Geocoder().geocode({ 'address': address }, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK && !!results[0]) {
-        map.fitBounds(results[0].geometry.viewport);
-        map.setCenter(results[0].geometry.location);
-        marker.setPosition(results[0].geometry.location);
+  var geocodeResults = function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK && !!results[0]) {
+      var result = results[0];
+      var location = result.geometry.location;
 
-        formattedAddressEl.value = results[0].formatted_address;
-        latEl.value = results[0].geometry.location.lat();
-        lngEl.value = results[0].geometry.location.lng();
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
+      map.fitBounds(result.geometry.viewport);
+      map.setCenter(location);
+      marker.setPosition(location);
+
+      updateValues(
+          location.lat(), 
+          location.lng(), 
+          result.formatted_address
+      );
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  }
+
+  var geocodeSearch = function(address) {
+    geocoder.geocode({ 'address': address }, geocodeResults);
   };
+
+  var updateValues = function(lat, lng, address) {
+    formattedAddressEl.value = address;
+    latEl.value = lat;
+    lngEl.value = lng;
+  }
   
   var searchTimeout;
   formattedAddressEl.addEventListener('keyup', function() {
@@ -53,5 +68,14 @@ function googleMapify(formattedAddressId, mapId, latId, lngId, defaultLat, defau
     searchTimeout = setTimeout( function() { 
       geocodeSearch(address);
     }, 750);
+  });
+
+  google.maps.event.addListener(marker, "dragend", function () {
+    var latlng = { 
+      lat: marker.getPosition().lat(),
+      lng: marker.getPosition().lng()
+    };
+
+    geocoder.geocode({ 'location': latlng }, geocodeResults);
   });
 };
